62231ea7587e6f3ba97c359b0d6d0a3dc48d1b6c
earguedast <este.at@gmail.com>
2025-11-24 17:23:33 -0600

    Create install-amd-drivers-2204.sh

---
+++ install-amd-drivers-2204.sh
@@ -0,0 +1,41 @@
+sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
+
+
+## 22.04LTS
+#ROCm installation
+
+
+wget https://repo.radeon.com/amdgpu-install/7.1/ubuntu/jammy/amdgpu-install_7.1.70100-1_all.deb
+sudo chown -Rv _apt:root /var/cache/apt/archives/partial/
+sudo chmod -Rv 700 /var/cache/apt/archives/partial/
+sudo apt install ./amdgpu-install_7.1.70100-1_all.deb -y
+sudo apt update
+sudo apt install python3-setuptools python3-wheel
+sudo usermod -a -G render,video $LOGNAME # Add the current user to the render and video groups
+sudo apt install rocm -y
+
+sudo apt update
+sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
+sudo apt install amdgpu-dkms -y
+
+sudo apt install python3-setuptools python3-wheel -y
+sudo apt install environment-modules -y
+sudo usermod -a -G video,render $LOGNAME
+echo 'ADD_EXTRA_GROUPS=1' | sudo tee -a /etc/adduser.conf
+echo 'EXTRA_GROUPS=video' | sudo tee -a /etc/adduser.conf
+echo 'EXTRA_GROUPS=render' | sudo tee -a /etc/adduser.conf
+
+#Grant GPU access to all users on the system
+sudo apt update
+wget https://repo.radeon.com/amdgpu/30.10.1/ubuntu/pool/main/a/amdgpu-insecure-instinct-udev-rules/amdgpu-insecure-instinct-udev-rules_30.10.1.0-2212064.22.04_all.deb
+sudo chown -Rv _apt:root /var/cache/apt/archives/partial/
+sudo chmod -Rv 700 /var/cache/apt/archives/partial/
+sudo apt install ./amdgpu-insecure-instinct-udev-rules_30.10.1.0-2212064.22.04_all.deb
+
+
+sudo groupadd devteam
+sudo usermod -a -G devteam cpoc
+sudo usermod -a -G render cpoc
+sudo usermod -a -G video cpoc
+sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo reboot
